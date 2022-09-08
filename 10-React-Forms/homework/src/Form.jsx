153839a56e6044d076fd371c9c14a9dccfd68e44
Facuzanan88 <facundozanandrea@gmail.com>
2022-09-07 21:16:12 -0300

    Forms M2

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -1,44 +1,48 @@
 import React from 'react';
 
-export default function Form() {
-  const [data, setData] = React.useState({
-    username: '',
-    password: '',
-  });
+export default function  Form() {
+const [input, setInput] = React.useState({
+  username: '',
+  password: '',
+});
 
-  const [errors, setErrors] = React.useState({});
+const [errors, setErrors] = React.useState({});
 
-  function handleDataChange(e) {
-    const newData = {...data, [e.target.name]: e.target.value};
-    setData(newData);
-    setErrors(validate(newData));
-  }
+function handleInputChange(e) {
+  const {name, value} = e.target;
+  setInput({...input, [name]: value})
+
+  setErrors(validate({...input, [name]: value}));
+};
 
 
   return (
     <form>
-      <div>
-        <label>Username:</label>
-        <input
-          type="text"
-          name="username"
-          value={data.username}
-          onChange={handleDataChange}
-          className={errors.username ? "danger" : ""} />
-          {errors.username && <span className="danger">{errors.username}</span>}
-      </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={data.password}
-          onChange={handleDataChange}
-          className={errors.password ? "danger" : ""} />
-          {errors.password && <span className="danger">{errors.password}</span>}
-      </div>
-    </form>
-  )
+    <div>
+      <label>Username:</label>
+      <input 
+      type="text" 
+      name="username" 
+      value={input.username}
+      onChange={handleInputChange}
+      className={errors.username ? "danger" : ""} 
+      />
+      {errors.username && <span>{errors.username}</span>}
+    </div>
+    <div>
+      <label>Password:</label>
+      <input 
+      type="password" 
+      name="password" 
+      value={input.password}
+      onChange={handleInputChange} 
+      className={errors.password ? "danger" : ""} 
+      />
+         {errors.password && <span>{errors.password}</span>}
+    </div>
+    
+  </form>
+  );
 }
 
 export function validate(input) {
@@ -48,79 +52,13 @@ export function validate(input) {
   } else if (!/\S+@\S+\.\S+/.test(input.username)) {
     errors.username = 'Username is invalid';
   }
+
   if (!input.password) {
     errors.password = 'Password is required';
   } else if (!/(?=.*[0-9])/.test(input.password)) {
     errors.password = 'Password is invalid';
   }
+  
 
   return errors;
-}
-// METODO CLASS
-// 
-// // export default Class Form extends React.Component {
-//   constructor(props) {
-//   super(props);
-// this.state = {
-// data: {
-// username:'',
-// password:'',
-//},
-// errors:{},
-//}
-//}
-// 
-// handleOnChange = (e) => {
-//  const {name, value} = e.target;
-// const newData = {...this.state.data, [name]: value}
-//
-// this.setState({
-//  data: newData,
-//  errors: validate (newData),
-//});
-//}
-//
-//
-//    render() {
-//  //  return (
- //    <form>
- //      <div>
-//         <label>Username:</label>
-//        <input
-// //         type="text"
-//          name="username"
-//          value={this.state.data.username}
-//          onChange={this.handleDataChange}
-//          className={this.state.errors.username ? "danger" : ""} />
-//          {this.state.errors.username && <span className="danger">{this.state.errors.username}</span>}
-//      </div>
-//      <div>
-//        <label>Password:</label>
-// //       <input
-//          type="password"
-//          name="password"
-//          value={this.state.data.password}
-//          onChange={this.handleDataChange}
-//          className={this.state.errors.password ? "danger" : ""} />
-//          {this.state.errors.password && <span className="danger">{this.state.errors.password}</span>}
-//      </div>
-//    </form>
-//  )
-//  }
-// }
-// 
-// export function validate(input) {
-//   let errors = {};
-//   if (!input.username) {
-//     errors.username = 'Username is required';
-//   } else if (!/\S+@\S+\.\S+/.test(input.username)) {
-//  //    errors.username = 'Username is invalid';
-//   }
-//   if (!input.password) {
-//     errors.password = 'Password is required';
-//   } else if (!/(?=.*[0-9])/.test(input.password)) {
-//     errors.password = 'Password is invalid';
-//   }
-//   
-//   return errors;
-// }
+};
