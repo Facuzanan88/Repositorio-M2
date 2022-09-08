78a53f3542934721ec2bdeb98f5d8fd244d8aac8
Facuzanan88 <facundozanandrea@gmail.com>
2022-09-07 21:41:33 -0300

    React Estado LifeCycle

--- 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import Logo from '../logoHenry.png'
 import SearchBar from './SearchBar.jsx';
 import './Nav.css';
+import texto from './Nav.css';
 
 function Nav({onSearch}) {
   return (
-    <div>Nav</div>
+    <nav class="navbar navbar-dark bg-dark">
+      <img src={Logo} alt=''/>
+     <span className="texto"> Weather - Henry App </span>
+      <SearchBar onSearch={onSearch} />
+    </nav>
   );
 };
 
--- 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 
 export default function SearchBar({onSearch}) {
+const [city, setCity] = useState('');
+
+function handleInputChange(e) {
+  setCity(e.target.value);
+}
+
   return (
     <form onSubmit={(e) => {
       e.preventDefault();
-      onSearch("Cairns");
+      onSearch(city);
+      setCity('');
     }}>
       <input
         type="text"
         placeholder="Ciudad..."
+        value={city}
+        onChange={handleInputChange}
       />
       <input type="submit" value="Agregar" />
     </form>
